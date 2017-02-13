0f2ba541370f5f266dd30ec3c0a7d155ad15a7d7
Russ Wells <russ@russdigital.com>
2017-02-12 21:51:29 -0800

    production complile fix 3

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -134,10 +134,6 @@ module.exports = {
         loader: 'babel',
 
       },
-      {
-        test: /\.(scss|sass)$/,
-        loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
-      },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
@@ -154,6 +150,10 @@ module.exports = {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
