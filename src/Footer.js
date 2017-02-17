08fec4fd735ec3a3ae4cf073dfc81043dcf45917
Russ Wells <russ@russdigital.com>
2017-02-16 17:44:33 -0800

    linked bootstrap css and edited footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,15 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        footer
+        <div className="Menu">
+          <IndexLink to='/' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='MenuItem'>
+            <div className="Title">EMLABS</div>
+          </IndexLink>
+          <Link to='/lab' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='MenuItem'>Lab</Link>
+          <Link to='/team' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='MenuItem'>Team</Link>
+          <Link to='/clients' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='MenuItem'>Clients</Link>
+          <Link to='/contact' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='MenuItem'>Contact</Link>
+        </div>
       </div>
     );
   }
